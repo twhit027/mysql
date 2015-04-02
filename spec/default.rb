7210857eafb1de03abe29f1154aee31e7af9e976
U-US\TreWhite <TreWhite@GMTI-ORF-105663.us.ad.gannett.com>
2015-04-02 13:36:57 -0400

    adding default recipe

---
+++ spec/default.rb
@@ -0,0 +1,12 @@
+mysql_service 'default' do
+  port '3306'
+  version '5.5'
+  initial_root_password 'change me'
+  action [:create, :start]
+end
+
+mysql_config 'default' do
+  source 'mysite.cnf.erb'
+  notifies :restart, 'mysql_service[default]'
+  action :create
+end
